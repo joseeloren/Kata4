d6a504b83c0c2774ffd63cc5e02cd89b6ae0971a
usuario <usuario@10.22.143.13>
2015-11-05 10:21:12 +0000

    Fourth commit

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -1,45 +1,15 @@
 package kata4;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Date;
+import java.util.ArrayList;
 
 public class Kata4 {
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        /*
-        String path = "C:\\Users\\usuario\\Documents";
-        File file = new File(path);
-        String[] fileList = file.list();
-        
-        print(file.listFiles(),"");
-        */
-        
-        String from = "C:\\Users\\usuario\\Downloads\\todo.pdf";
-        String to = "C:\\Users\\usuario\\Downloads\\todo(1).pdf";
-        BufferedInputStream input = new BufferedInputStream(new FileInputStream(new File(from)));
-        BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(new File(to)));
-        
-        byte[] buffer = new byte[1024];
-        
-        Date date = new Date();
-        
-        while (input.read(buffer)>= 0)
-            output.write(buffer);
-        input.close();
-        output.close();
-        
-        System.out.println((new Date()).getTime() - date.getTime() + " msg");
-    }
-    
-    public static void print(File[] fileList, String spaces){
-        for (File f: fileList) {
-            System.out.println(spaces + ((f.isDirectory()) ? "+": "")+ f.getName());
-            if (f.isDirectory()) print(f.listFiles(), spaces + " ");
-        }
+        String name = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\Kata4\\data\\emailsfilev1.txt";
+        ArrayList<String> mailList = MailListReader.read(name);
+        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
+        HistogramDisplay histogramDisplay = new HistogramDisplay(histogram);
+        histogramDisplay.execute();
     }
 }
