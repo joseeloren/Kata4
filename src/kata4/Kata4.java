c273015e42e68e77bf4440edee30040add6c67a2
usuario <usuario@10.22.143.13>
2015-11-05 09:16:57 +0000

    Second commit

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -1,14 +1,40 @@
 package kata4;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Date;
 
 public class Kata4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        /*
         String path = "C:\\Users\\usuario\\Documents";
         File file = new File(path);
         String[] fileList = file.list();
         
         print(file.listFiles(),"");
+        */
+        
+        String from = "C:\\Users\\usuario\\Downloads\\todo.pdf";
+        String to = "C:\\Users\\usuario\\Downloads\\todo(1).pdf";
+        BufferedInputStream input = new BufferedInputStream(new FileInputStream(new File(from)));
+        BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(new File(to)));
+        
+        byte[] buffer = new byte[1024];
+        
+        Date date = new Date();
+        
+        int read;
+        while ((read = input.read(buffer))>= 0)
+            output.write(buffer);
+        input.close();
+        output.close();
+        
+        System.out.println((new Date()).getTime() - date.getTime() + " msg");
     }
     
     public static void print(File[] fileList, String spaces){
